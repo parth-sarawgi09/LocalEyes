e9f417137fe6a4d25a06e47b512b3ce38c548c33
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2025-12-01 23:35:32 +0530

    feat: completed authentication (signup, login, JWT, middleware)

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import {signup} from '../controllers/authController.js';
 import {login} from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();    
 
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.get("/me", protect, (req, res) => {
+    res.json({
+        message: "Protected route accessed",
+        user: req.user
+    });
+});
 
 export default router;
