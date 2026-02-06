7435f8234601b9a7bed5c8197fdc6309d7b22d02
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2026-02-06 10:56:59 +0530

    minor updated

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -3,6 +3,7 @@ import { protect } from "../middleware/authMiddleware.js";
 import {
   createBooking,
   getGuideBookings,
+  updateBookingStatus,
 } from "../controllers/bookingController.js";
 
 const router = express.Router();
@@ -12,5 +13,8 @@ router.post("/", protect, createBooking);
 
 // Guide views their bookings
 router.get("/guide", protect, getGuideBookings);
+
+//guide accept / reject the bookings
+router.patch("/:bookingId", protect, updateBookingStatus);
 
 export default router;
